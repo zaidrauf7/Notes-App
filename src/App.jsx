c0874ff5676c5096f489478aa2fce493f2ce1516
zaidrauf7 <iamzaidrauf@gmail.com>
2024-06-06 15:13:01 +0500

    install react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { Button } from './components/ui/button'
+import { BrowserRouter, Routes } from 'react-router-dom'
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+  <BrowserRouter>
+  <Routes>
+  
+  </Routes>
+  </BrowserRouter>
   )
 }
 
